9128214a806c20cc744fdc94ad916627e4890a8f
wen <wen09210@gmail.com>
2025-01-05 16:43:10 +0800

    feat: ch3

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css';
+import { inter, lusitana } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
@@ -6,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+          <body className={`${lusitana.className} antialiased`}>{children}</body>
     </html>
   );
 }
