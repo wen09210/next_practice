60f1f601cbd075eb158ad1b49d0c41b1588617b7
wen <wen09210@gmail.com>
2025-03-02 16:30:53 +0800

    feat: ch16 seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
+import { Metadata } from 'next';
 import '@/app/ui/global.css';
 import { lusitana } from '@/app/ui/fonts';
 // import { error } from 'console';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme 看板',
+    default: 'Acme 看板',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+ 
 export default function RootLayout({
   children,
 }: {
